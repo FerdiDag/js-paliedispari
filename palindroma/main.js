49abb14464ff4bdfe02e5d09b02aaa0690cd2ebc
Ferdinando <ferdi.dagostino@gmail.com>
2020-04-24 14:51:55 +0200

    forse finito esercizio palindromi

--- palindroma/main.js
+++ palindroma/main.js
@@ -7,15 +7,18 @@ function ispalin() {
     //chiedo all'utente una parola
     var paroladivisa = prompt('Inserisci una parola');
     console.log(paroladivisa);
-    // prendo la parola inserita dall'utente e la inserisco splittata all'interno di un array
+    // prendo la parola inserita dall'utente e la inserisco splittata all'interno di un array(per poter utilizzare la funzione reverse sugli array), e la ritrasdormo in stringa
     var paroladivisa = paroladivisa.split('');
-    console.log(paroladivisa);
-    //utilizzo la funzione reverse per invertire l'ordine delle lettere
+    var parolaUnita = paroladivisa.join('');
+    console.log(parolaUnita);
+    //utilizzo la funzione reverse per invertire l'ordine degli elemnti dell'array, e ristrasformo in stringa
     var paroladivisa2 = paroladivisa.reverse();
-    console.log(paroladivisa2);
-    // reinserisco le due parole in una stringa e verifico se sono palindrome con if ?
-var parolaUnita = paroladivisa.join();
-var parolaUnita2 = paroladivisa2.join();
-
-
+    var parolaUnita2 = paroladivisa2.join('');
+    console.log(parolaUnita2);
+    // confronto se le parole sono palindrome 
+    if (parolaUnita == parolaUnita2) {
+        alert('la parola è palindroma');
+    } else {
+        alert('la parola NON è palindroma');
+    }
 }
