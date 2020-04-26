46ec18cf2dc39af659a5d44b14d9c02d183defed
Ferdinando <ferdi.dagostino@gmail.com>
2020-04-26 16:29:40 +0200

    esercizio palindromo - richiesta parola al di fuori della funzione

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,6 +1,7 @@
 //palindroma: scrivere una funzione per capire se una parola è palindroma. Quindi chiedere una parola all'utente, utilizzare la funzione per sapere se la parola inserita è palindroma e stampare un messaggio appropriato
 
 
+
 ispalin();
 
 function ispalin() {
@@ -18,6 +19,30 @@ function ispalin() {
         alert('la parola NON è palindroma');
     }
 }
+
+
+// //chiedo all'utente una parola
+// var parola = prompt('Inserisci una parola');
+// ispalin();
+//
+// function ispalin() {
+//     parola = parola.toLowerCase();
+//     console.log(parola);
+//     // prendo la parola inserita dall'utente e la inserisco splittata all'interno di un array(per poter utilizzare la funzione reverse sugli array), e la ritrasdormo in stringa
+//     var reverse = parola.split('').reverse().join('');
+//
+//     //confronto se le parole sono palindrome
+//     if (parola == reverse) {
+//         alert('la parola è palindroma');
+//     } else {
+//         alert('la parola NON è palindroma');
+//     }
+// }
+//
+
+
+
+
 
 // var parolaUnita = paroladivisa.join('');
 // console.log(parolaUnita);
